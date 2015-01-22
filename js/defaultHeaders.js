ed16624fda24efecae418792a497e59fab4ad870
Fredrick Jones <fredrick.w.jones@gmail.com>
2015-01-21 17:35:32 -0700

    finished

--- js/defaultHeaders.js
+++ js/defaultHeaders.js
@@ -1,9 +1,10 @@
+'use strict';
 var app = angular.module('chatroom');
 
 app.factory('httpRequestInterceptor', function () {
   return {
     request: function (config) {
-      config.headers = {'X-Parse-Application-Id': '5oA4oab0RNwHkfRrXt5WlMqDnzUu6bcmpf4P2uKy', 'X-Parse-REST-API-Key': 'ZIKuhgCABMrP0kLnSMlEYWYlljNWtHnJzYaKwWc8'}
+      config.headers = {'X-Parse-Application-Id': '5oA4oab0RNwHkfRrXt5WlMqDnzUu6bcmpf4P2uKy', 'X-Parse-REST-API-Key': 'ZIKuhgCABMrP0kLnSMlEYWYlljNWtHnJzYaKwWc8'};
       return config;
     }
   };
